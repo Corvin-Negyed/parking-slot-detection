2c048a5fa0ef4d5c7e6aa7c1f589444d8cbf76f8
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-26 00:46:03 +0300

    Add file input and browse button references

--- static/app.js
+++ static/app.js
@@ -1,3 +1,5 @@
 // Get DOM elements
 const dropZone = document.getElementById('dropZone');
+const fileInput = document.getElementById('fileInput');
+const browseBtn = document.getElementById('browseBtn');
 
