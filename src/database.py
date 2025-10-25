1930e04be52a7fa185ec45c179b859014106e4af
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-25 23:57:30 +0300

    Add status column to parking_events

--- src/database.py
+++ src/database.py
@@ -20,7 +20,8 @@ def init_database():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS parking_events (
             id SERIAL PRIMARY KEY,
-            spot_id INTEGER NOT NULL
+            spot_id INTEGER NOT NULL,
+            status VARCHAR(20) NOT NULL
         )
     ''')
 
