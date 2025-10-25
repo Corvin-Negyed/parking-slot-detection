b21c28be0e2eee30cbc7ddbc7f18a04bf9faf83b
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-25 23:48:28 +0300

    Add database user configuration

--- src/config.py
+++ src/config.py
@@ -8,4 +8,5 @@ class Config:
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '5432')
     DB_NAME = os.getenv('DB_NAME', 'parking_db')
+    DB_USER = os.getenv('DB_USER', 'postgres')
 
