62a1b5e1da5c42bbffd01a69fc2f355cd6f12714
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-25 23:52:32 +0300

    Add model path configuration

--- src/config.py
+++ src/config.py
@@ -13,4 +13,6 @@ class Config:
     
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
     FLASK_PORT = int(os.getenv('FLASK_PORT', '5000'))
+    
+    MODEL_PATH = os.getenv('MODEL_PATH', 'Models/yolov8m mAp 48/weights/best.pt')
 
