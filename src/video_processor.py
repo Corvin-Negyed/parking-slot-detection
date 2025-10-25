a23778510427169be11154d735e0094956ac26a4
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-26 00:06:05 +0300

    Initialize detector in VideoProcessor

--- src/video_processor.py
+++ src/video_processor.py
@@ -5,4 +5,5 @@ from src.detector import ParkingDetector
 class VideoProcessor:
     def __init__(self, video_path):
         self.video_path = video_path
+        self.detector = ParkingDetector()
 
