706989f07d121f0417287939df569ff22e9d5ee4
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-25 23:55:34 +0300

    Implement database connection function

--- src/database.py
+++ src/database.py
@@ -3,5 +3,12 @@ from src.config import Config
 
 
 def get_connection():
-    pass
+    conn = psycopg2.connect(
+        host=Config.DB_HOST,
+        port=Config.DB_PORT,
+        database=Config.DB_NAME,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD
+    )
+    return conn
 
