1ec790352a0b84d2990f69dd0c351f73994b7c60
Corvin-Negyed <lahmibisayni@gmail.com>
2025-10-26 00:59:45 +0300

    Add upload endpoint

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    return jsonify({'status': 'success'})
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
